a66a93a2a769716efe7269821ca7865bc31dd3e5
Aaron SHepard <asdantius5@gmail.com>
2012-05-17 01:10:53 -0700

    added actionbar

--- src/edu/ucsb/cs/cs185/aaronshepard/HCI_Prog5_ImageActivity.java
+++ src/edu/ucsb/cs/cs185/aaronshepard/HCI_Prog5_ImageActivity.java
@@ -1,13 +1,88 @@
 package edu.ucsb.cs.cs185.aaronshepard;
 
-import android.app.Activity;
-import android.os.Bundle;
+import com.example.android.actionbarcompat.ActionBarActivity;
+import com.example.android.actionbarcompat.R;
 
-public class HCI_Prog5_ImageActivity extends Activity {
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class HCI_Prog5_ImageActivity extends ActionBarActivity {
+	private final static int SELECT_PICTURE = 1;
+	
+	private String m_picture_path;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        m_picture_path = "";
+        
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+
+        // Calling super after populating the menu is necessary here to ensure that the
+        // action bar helpers have a chance to handle this event.
+        return super.onCreateOptionsMenu(menu);
+    }
+    
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                m_picture_path = getPath(selectedImageUri);
+            }
+        }
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Toast.makeText(this, "Tapped home", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menu_picture:
+                Toast.makeText(this, "Picture...", Toast.LENGTH_SHORT).show();
+                // in onCreate or any event where your want the user to
+                // select a file
+                //http://stackoverflow.com/questions/2169649/open-an-image-in-androids-built-in-gallery-app-programmatically
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), SELECT_PICTURE);
+                break;
+
+            case R.id.menu_settings:
+                Toast.makeText(this, "Tapped settings", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.menu_help:
+                Toast.makeText(this, "Tapped help", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public String getPath(Uri uri) {
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        int column_index = cursor
+                .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(column_index);
     }
 }
